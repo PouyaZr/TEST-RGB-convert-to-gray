6ebd6fc23c2631e716b8622d66a9ea3ea5e550a9
PouyaZr <pouya.zarrinkhat@gmail.com>
2022-03-27 13:47:06 +0430

    The Initial Code

---
+++ RGB-to-gray.py
@@ -0,0 +1,20 @@
+import cv2
+
+img = cv2.imread("Wall-E.jpg")
+
+img.shape
+
+gray1 = img[:, :, 0]
+
+gray2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("my img", img)
+cv2.imshow("gray1", gray1)
+cv2.imshow("gray2", gray2)
+
+cv2.imwrite("Wall-E_gray.jpg", gray2)
+
+
+
+cv2.waitKey()
+cv2.destroyAllWindows()
